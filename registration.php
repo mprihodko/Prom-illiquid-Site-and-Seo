ecb459e8cb13009771f8638ff81b3d1e2d331fac
mprihodko <prihodkomaks92@gmail.com>
2015-04-21 09:44:01 +0300

    Maks 21/04/15

--- registration.php
+++ registration.php
@@ -712,14 +712,9 @@
     </body>
 </html>
 <?php
-require_once 'application/db/MyPDO.php';
-$conn= new PDO_connect();
-$conn->db_connect();
+require_once 'application/register/register.php';
 if (isset($_POST['register'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $company = $_POST['company'];
-    $sql = $conn->dbh->query("INSERT INTO user (name, email, password, company) VALUES ('$name', '$email', '$password', '$company')");
+  $reg=new Registration();
+  $reg->register();
 }
 ?>﻿
